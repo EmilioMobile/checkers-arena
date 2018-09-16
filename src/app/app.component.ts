0d2f98f477b3a0223ddcf22e4da5ed9aff2c1fef
Emilio Jose Coronado Lopez <emilio.mobile@gmail.com>
2018-09-16 15:38:54 +0900

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'scoreboard-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'scoreboard';
+}
